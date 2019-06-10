2f2f7336108edd3901a17775416650def8d58f9d
Ervin Smajic <you@example.com>
2019-06-10 11:23:02 -0700

    having problems with git

--- src/TaskManagementTest.java
+++ src/TaskManagementTest.java
@@ -9,11 +9,15 @@ public class TaskManagementTest {
 	
 	TaskManagement tm;
 	Account acc;
+	TaskToDo ttd;
 	ArrayList<Account> accounts = new ArrayList<>();
+	ArrayList<TaskToDo> tasks = new ArrayList<>();
 	
 	@Before
 	public void setUp() {
 		tm = new TaskManagement();
+		ttd = new TaskToDo("Ervin", "coding", null, true);
+		tasks.add(ttd);
 		acc = new Account(500, "1234");
 		accounts.add(acc);
 		
@@ -30,10 +34,18 @@ public class TaskManagementTest {
 		boolean condition = tm.logIn(500, "1234", accounts);
 		assertTrue(condition);
 	}
+	
 	@Test
 	public void shouldReturnTrueWhenPasswordIsFourDigits() {
 		boolean condition = tm.passwordNumeric("1234");
 		assertTrue(condition);
 	}
+	
+	@Test
+	public void shouldReturnTrueWhenNumberOfTaggedTasksExists() {
+		int result = tm.numberOfTaggedTasks(tasks);
+		assertEquals(1, result);
+	}
+
 
 }
